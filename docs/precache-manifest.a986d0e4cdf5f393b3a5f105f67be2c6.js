e16d2108dc14997c104e43382d433ea28dd15ae3
oleksii.hopchenko@sigma.software <oleksii.hopchenko@sigma.software>
2020-07-21 23:23:07 +0300

    added docs

---
+++ docs/precache-manifest.a986d0e4cdf5f393b3a5f105f67be2c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa9b6b2b84cdda060a3",
+    "url": "build/2.3aa9b6b2.js"
+  },
+  {
+    "revision": "946da9b4d84d98e429beadd72922082e",
+    "url": "build/2.3aa9b6b2.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46008162eaa4d8d14fe",
+    "url": "build/bundle.e4600816.js"
+  },
+  {
+    "revision": "022c75c8a95fd57c96b9",
+    "url": "build/main.022c75c8.js"
+  },
+  {
+    "revision": "b7a893b48331d6b49182a32f16ebd608",
+    "url": "index.html"
+  },
+  {
+    "revision": "022c75c8a95fd57c96b9",
+    "url": "static/css/main.348c4e21.chunk.css"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "e100cb3858b2e4be3333156aa979bf66",
+    "url": "static/media/avatar.e100cb38.png"
+  }
+]);
